2562e1a668b86634bda6414cdb5ff7caaccec638
notolq <206822596+torikara-code@users.noreply.github.com>
2025-10-24 20:49:58 +0900

    ボタンのCSSをTailwindの記述に修正

--- src/components/Button/ReactButton.tsx
+++ src/components/Button/ReactButton.tsx
@@ -4,7 +4,19 @@ const ReactButton: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
+    <button
+      className="rounded-lg
+      border border-transparent
+      px-4 py-2
+      text-base font-medium font-inherit
+    bg-gray-50
+      cursor-pointer
+      transition-colors duration-200
+      hover:border-indigo-500
+      focus:outline-[4px] focus:outline-auto focus:outline-webkit-focus-ring-color
+      dark:bg-[#1a1a1a]"
+      onClick={() => setCount((count) => count + 1)}
+    >
       count is {count}
     </button>
   );
